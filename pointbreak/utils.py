1e146f7e1087264217c9cdfc3365abde0cf402ee
Tony Simpson <agjasimpson@gmail.com>
2018-07-12 11:23:32 +0100

    Added a test for preexiting breakpoints
    It doesn't work as I expected so its commented out. A better
    understanding is required.

---
+++ pointbreak/utils.py
@@ -0,0 +1,21 @@
+import ctypes
+import mmap
+
+
+def create_callable_from_machine_code(machine_code, doc=None, restype=None, argtypes=None, use_errno=False, use_last_error=False):
+    if argtypes is None:
+        argtypes = []
+    exec_code = mmap.mmap(-1, len(machine_code), prot=mmap.PROT_WRITE | mmap.PROT_READ | mmap.PROT_EXEC)
+    exec_code.write(machine_code)
+    c_type = ctypes.c_byte * len(machine_code)
+    c_var = c_type.from_buffer(exec_code)
+    address = ctypes.addressof(c_var)
+    c_func_factory = ctypes.CFUNCTYPE(restype, *argtypes, use_errno=use_errno, use_last_error=use_last_error)
+    func = c_func_factory(address)
+    func._exec_code = exec_code # prevent GC of code
+    func.__doc__ = doc
+    return func
+
+
+breakpoint = create_callable_from_machine_code('\xCC\xC3')
+
--- tests/test_break_point_interface.py
+++ tests/test_break_point_interface.py
@@ -1,4 +1,5 @@
 import pointbreak
+import pytest
 
 def test_breakpoint_on_main():
     db = pointbreak.create_debugger('python', '--version', timeout=0.5)
@@ -8,3 +9,12 @@ def test_breakpoint_on_main():
         db.continue_none_stop()
     assert event.name == pointbreak.EVENT_NAME_TRAP
     assert bp in event.triggered, "break point should be triggered"
+
+@pytest.mark.skip(reason="We get a STOP Event before the TRAP - need to research why it behaves differently")
+def test_existing_breakpoint_in_program():
+    db = pointbreak.create_debugger('python', '-c', 'import pointbreak.utils; pointbreak.utils.breakpoint()', timeout=0.5)
+    event = db.next_event()
+    assert event.name == pointbreak.EVENT_NAME_TRAP
+    if not event.is_last_event():
+        db.continue_none_stop()
+
