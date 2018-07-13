f868c484db18b7717dfc60bacef6869fe605a2de
Tony Simpson <agjasimpson@gmail.com>
2018-07-13 13:25:12 +0100

    Python Packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="pointbreak",
     version="0.0.1",
+    author="Tony Simpson",
+    author_email="agjasimpson@gmail.com",
+    description="System process debug and analysis library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tonysimpson/pointbreak",
     install_requires=["distorm3", "pyptrace", "pyelftools"],
-    packages=setuptools.find_packages(),
     setup_requires=['pytest-runner'],
-    tests_require=['pytest']
+    tests_require=['pytest'],
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    )
 )
 
