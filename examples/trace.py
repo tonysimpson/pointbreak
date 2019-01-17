2694a99711280f525653887462135682deedca2e
Tony Simpson <agjasimpson@gmail.com>
2019-01-17 20:38:55 +0000

    No a good example and probably broken

--- examples/trace.py
+++ examples/trace.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import pointbreak
 
 
-# XXX the symbols interface and its interaction wiht breakpoints and traps 
+# XXX the symbols interface and its interaction with breakpoints and traps 
 # are WIP
 class Tracer:
     def __init__(self):
@@ -19,13 +19,14 @@ class Tracer:
             except:
                 name = 'Unknown'
             self._name_cache[address] = name
-        if len(db.backtrace()) == 2:
-            print(name)
-#            print("{}{}{}".format(" " * self.depth, name, db.backtrace()))
+            print("{}{}".format(" " * self.depth, name))
         self.depth += 1
+        depth = self.depth
         def trace_exit(db):
-            self.depth -= 1
-            return False
+            if self.depth == depth: # because of recursion we can have more than one bp on the return address
+                self.depth -= 1
+                return False
+            return True
         if name != '_start': # _start doesn't have a return address - interesting :D
             return_addres = db.stack[0]
             db.add_breakpoint(return_addres, trace_exit, immediately=True)
